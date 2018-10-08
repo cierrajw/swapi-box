f61c8d769b1810ba0617713c9bcc7ce0b8962794
Cierra Washington <cierrajwashi@gmail.com>
2018-10-08 16:25:59 -0600

    Push up latest changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,14 @@ import LandingPage from '../LandingPage/LandingPage';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      allCards: [],
+      favoriteCards: []
+    }
+  }
   render() {
     return (
       <div className="App">
