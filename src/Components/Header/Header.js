ebd03977a8e67c91f5205cb0b866a90b953e8049
Cierra Washington <cierrajwashi@gmail.com>
2018-10-09 17:04:29 -0600

    Fix page layout issue

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
 
   return (
     <div className="header-div">
-    The Header
+    <section className="header-section">
+    <h1 className="swapi-title">Swapi Box</h1>
+    </section>
+    <button className="favorite">Favorites</button>
     </div>
   );
 }
