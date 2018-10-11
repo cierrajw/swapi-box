ac3c078379c2c1412986e61a1cae93baa1e099e8
Cierra Washington <cierrajwashi@gmail.com>
2018-10-11 16:02:51 -0600

    add and edit tests, card container etc.

---
+++ src/Components/LandingPage/landing-page.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import LandingPage from './LandingPage.js';
+
+describe('Landing Page', () => {
+
+  it('should match the snapshot', () => {
+
+    const wrapper = shallow(<LandingPage />);
+
+    expect(wrapper).toMatchSnapshot();
+
+  });
+
+});
