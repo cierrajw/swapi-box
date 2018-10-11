8fce42eff6b3357df703edeadde56f93636a1b87
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-11 11:28:19 -0600

    Refactor code to return languages for people

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
 
   return (
     <div className="header-div">
-    The Header
+    <section className="header-section">
+    <h1 className="swapi-title">SWAPI BOX</h1>
+    <button className="favorites-button">Favorites</button>
+    </section>
     </div>
   );
 }
