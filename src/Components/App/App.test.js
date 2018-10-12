368dd44c9f8f498a8cb23a056aecc245df299b98
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-12 13:31:49 -0600

    Add initial tests for App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -11,15 +11,31 @@ describe('App', () => {
   beforeEach(() => {
     wrapper=shallow(<App />);
     mockEvent=jest.fn();
-  })
+  });
 
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   }); 
 
   it('should invoke displayFilmText on page load', () => {
-    wrapper.instance().displayFilmText(mockEvent);
+    wrapper.instance().displayFilmText();
     expect(mockEvent).toHaveBeenCalled();
+  });
+
+  it('should update state when displayPeopleCards is invoked', () => {
+    
+  });
+
+  it('should call fetch with correct parameters in displayFilmText', () => {
+
+  });
+
+  it('should update state when displayFilms is invoked', () => {
+
+  });
+
+  it('should update state when setRedistrict is invoked', () => {
+    
   })
   
 })
