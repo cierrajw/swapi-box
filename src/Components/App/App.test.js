21d650344d15209b577a2649f03e3e51fda309b1
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-12 15:03:17 -0600

    Add App tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -23,19 +23,33 @@ describe('App', () => {
   });
 
   it('should update state when displayPeopleCards is invoked', () => {
-    
+    const initialState = [];
+    const expected = [{name: 'Luke Skywalker', species: 'human'}];
+    wrapper.instance().displayPeopleCards();
+    expect(wrapper.state('peopleCards')).toEqual(expected);
   });
 
   it('should call fetch with correct parameters in displayFilmText', () => {
-
+    const url = 'https://swapi.co/api/films/';
+    wrapper.instance().displayFilmText();
+    expect(window.fetch).toHaveBeenCalledWith(url); 
   });
 
   it('should update state when displayFilms is invoked', () => {
+    const initialState = '';
+    const expected='Star Wars movie text';
+    wrapper.instance().displayFilms();
+    wrapper.setState({filmText: 'Star Wars movie text'});
+    expect(wrapper.state('filmText')).toEqual(expected);
 
   });
 
   it('should update state when setRedistrict is invoked', () => {
-    
+    const initialState = false;
+    const expected = true;
+    wrapper.instance().setRedirect();
+    wrapper.setState({redirect: true});
+    expect(wrapper.state('redirect')).toEqual(expected);
   })
   
 })
