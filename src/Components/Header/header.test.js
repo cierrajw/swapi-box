4ec453e41c5e96de00b0045f8efb8d3fbdfec511
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-18 15:25:12 -0600

    Add tests for Header

--- src/Components/Header/header.test.js
+++ src/Components/Header/header.test.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ReactDOM from 'react-dom';
 import Header from './Header.js';
 
 describe('Header', () => {
 
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Header />);
+    const mockFavoriteCards = [{
+      favorite: true,
+      homeworld: "Tatooine",
+      id: 908692779067.1481,
+      language: "n/a",
+      name: "C-3PO",
+      population: "200000",
+      species: "Droid",
+      type: "people"
+    }]
+    const wrapper = shallow(<Header favoriteCards = {mockFavoriteCards} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call handleFavoritesClick when button is clicked', () => {
+    const favoriteCards = [];
+    const mockHandleFavorites = jest.fn();
+    const displayFavoritesMock = jest.fn();
+    const wrapper = shallow(<Header favoriteCards = {favoriteCards} displayFavorites={displayFavoritesMock} />);
+    wrapper.find('button').simulate('click');
+    expect(displayFavoritesMock).toHaveBeenCalled();
   });
 
 });
